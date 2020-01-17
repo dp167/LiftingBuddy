90e1d163fadf5c46053171394acffdac9fb32c34
David Poole <42785393+dp167@users.noreply.github.com>
2020-01-16 19:00:58 -0800

    initial commit

--- LiftingBuddy_test/ViewController.swift
+++ LiftingBuddy_test/ViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 10
+       }
+       
+       func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:"LifCell", for:indexPath)
+        return cell
+       }
 }
 
