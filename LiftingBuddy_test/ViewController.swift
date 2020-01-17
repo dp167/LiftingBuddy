e6cd38f9e883e613b80f8827606e3be5dfb3fe29
David Poole <42785393+dp167@users.noreply.github.com>
2020-01-16 18:37:20 -0800

    Initial Commit

---
+++ LiftingBuddy_test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiftingBuddy_test
+//
+//  Created by David Poole on 1/16/20.
+//  Copyright © 2020 David Poole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
